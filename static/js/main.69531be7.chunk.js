5638a6fae903df58813f10e66e56c9c5bf9bc304
PauloSilva96 <109427559+PauloSilva96@users.noreply.github.com>
2024-01-27 20:29:13 +0100

    Update main.69531be7.chunk.js

--- static/js/main.69531be7.chunk.js
+++ static/js/main.69531be7.chunk.js
@@ -538,7 +538,7 @@
                     var e, t = Object(a.useContext)(w),
                         n = t.account,
                         i = function(e) {
-                            return A("0x9C9d3e8B177F8D943343941326eBe43ec3612cC9", z, e)
+                            return A("0x7F837d41D743236a09C9183a3cC34d00319dB5a8", z, e)
                         }(t.signer),
                         r = Object(d.a)("(max-width:700px)"),
                         o = Object(a.useState)(""),
